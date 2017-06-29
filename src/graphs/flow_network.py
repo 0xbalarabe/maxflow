f30994ba877a29b8a7a0568ff78095ec6ddd80ce
balaogbeha <balaogbeha@gmail.com>
2017-06-29 13:45:54 +0100

    New Graph reading functionality.
    Laplacian construction from graph.

---
+++ src/graphs/flow_network.py
@@ -0,0 +1,16 @@
+__author__ = 'balaogbeha'
+
+import networkx as nx
+
+class FLowNetwork(object):
+
+    def __init__(self):
+        self.graph = nx.Graph()
+        self.source = 0
+        self.sink = 0
+
+    def set_source(self, source):
+        self.source = source
+
+    def set_sink(self, sink):
+        self.sink = sink
--- src/graphs/graph.py
+++ src/graphs/graph.py
@@ -1,1 +1,27 @@
 __author__ = 'balaogbeha'
+
+import scipy as sp
+import networkx as nx
+
+
+def compute_adjacency_matrix(graph):
+    return 0
+
+
+def compute_degree_matrix(graph):
+    return 0
+
+
+def compute_laplacian_matrix(graph):
+
+    d = compute_degree_matrix(graph)
+
+    a = compute_adjacency_matrix(graph)
+
+    return d - a
+
+
+
+def compute_residual_graph(graph, flow):
+    pass
+
